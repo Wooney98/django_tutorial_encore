e648364ba1ea4ae7b468b25db23ada79d855b69c
sws <109335745+Wooney98@users.noreply.github.com>
2022-11-29 17:50:42 +0900

    templates 폴더구조 변경, list table 수정

--- community/views.py
+++ community/views.py
@@ -34,6 +34,7 @@ def viewDetail(request, num=1):
     article_detail = Article.objects.get(id=num)
     return render(request, 'community/view_detail.html', {'article_detail':article_detail})
 
+
 def index(request):
     latest_article_list = Article.objects.all().order_by('-cdate')[:3]
     return render(request, "index.html", {'latest_article_list':latest_article_list})
