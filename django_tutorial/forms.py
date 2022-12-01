3a078f23c3932693d5c740c8ad52b50cec858e2a
sws <109335745+Wooney98@users.noreply.github.com>
2022-12-01 17:46:08 +0900

    로그아웃 템플릿 코딩, 회원가입기능 구현

--- django_tutorial/forms.py
+++ django_tutorial/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django import forms
 
 class CreateUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)#email 필드 추가
+    email = forms.EmailField(required=True) # email 필드 추가
     class Meta:
-        model = User # Django 제공 model DB
-        fields = ("username","email","password1","password2")
+        model = User # 장고 제공 model DB
+        fields = ("username", "email", "password1", "password2")
